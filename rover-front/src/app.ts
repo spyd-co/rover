1eba1043c4f8f7dd11278bacd43c0b461283e0ee
spyd-co <googl@outlook.com>
2023-12-10 15:29:45 +0800

    set canvas size

--- rover-front/src/app.ts
+++ rover-front/src/app.ts
@@ -6,8 +6,8 @@ class App {
     constructor() {
         // create the canvas html element and attach it to the webpage
         var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
+        //canvas.style.width = "100%";
+        //canvas.style.height = "100%";
         canvas.id = "gameCanvas";
         document.body.appendChild(canvas);
 
